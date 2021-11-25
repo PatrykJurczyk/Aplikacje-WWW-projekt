c8e3e14b13c6edb272e0a8db9daf6e24971aff43
Patryk1468 <jurczykpatryk8@gmail.com>
2021-11-25 15:08:09 +0100

    .

--- showpage.php
+++ showpage.php
@@ -1,19 +1,35 @@
 <?php
 function PokazPodstrone($db, $id) {
+    // Pierwszy sposób.
 
+    // $id_clear = htmlspecialchars($id);
+    // $result = $db->query("SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1");
+    
+    // $row = mysqli_fetch_array($result);
+
+    // if(empty($row['id']))
+    // {
+    //     $web = 'Nie znaleziono strony';
+    // }
+    // else
+    // {
+    //     $web = $row['page_content'];
+    // }
+
+    // return $web;
+
+    // Sposób obiektowo.
     $id_clear = htmlspecialchars($id);
-    $result = $db->query("SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1");
-    $row = mysqli_fetch_array($result);
+    $q = "SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1";
+    $result = $db->query($q);
 
-    if(empty($row['id']))
-    {
-        $web = 'Nie znaleziono strony';
+    while($obj = $result->fetch_object()){
+        if(empty($obj->id)){
+            $web = 'Nie znaleziono strony';
+        } else{
+            $web = $obj->page_content;
+        }
     }
-    else
-    {
-        $web = $row['page_content'];
-    }
-
     return $web;
 }
 ?>
